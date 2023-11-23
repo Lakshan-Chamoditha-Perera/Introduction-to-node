82a7949831ffb02950fdd67ccaac03ef06a4c299
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2023-11-23 23:53:38 +0530

    file read & write

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -1,12 +1,34 @@
-const fs = require('fs');
+const fs = require('fs'); //file system module
+const path = require('path'); //path module
+//we can use the fs module to read and write files on the server
 
+/*
 fs.readFile('./lorem.txt', (error, data) => {
+    if (error) throw error;
+    console.log(data.toString());
+})*/
+
+fs.readFile('./sample.txt', (error, data) => {
     if (error) throw error;
     console.log(data.toString());
 })
 
+console.log("hey there!") //this will run first because readFile is asynchronous
+
 //fs module error handling
-process.on('uncaughtException',error => {
+process.on('uncaughtException', error => {
     console.error(`There was an error:  ${error.message}`); //error message
-    process.exit(1); //exit with failure
+    // process.exit(1); //exit with failure
+});
+
+//fs module read file
+fs.readFile(path.join(__dirname, 'files', 'sample.txt'), (error, data) => {
+    if (error) throw error;
+    console.log(data.toString());
+})
+
+//fs module write file
+fs.writeFile(path.join(__dirname, 'files', 'sample.txt'), 'Nice to meet you.', error => {
+    if (error) throw error;
+    console.log('Write complete');
 })
